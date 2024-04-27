0ab03386ad28a6723505dcd09ee0d7f43a1dd7f1
harjotse <bhatiaharjot2000@gmail.com>
2024-04-27 22:55:01 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -129,20 +129,176 @@
 // console.log(score)
 // console.log(newScore.toFixed(5));
 // console.log(newScore.toString().length);
-const othernumber=123.5643
-console.log(othernumber.toPrecision(5)) // can give round off if req else no of dig that are req
-
-const num6=1000000
-console.log(num6.toLocaleString('en-IN'))
-
-
+// const othernumber=123.5643
+// console.log(othernumber.toPrecision(5)) // can give round off if req else no of dig that are req
+// const num6=1000000
+// console.log(num6.toLocaleString('en-IN'))
+//
 
 
 
+// date and time
+// let myDate=new Date()
+// console.log(myDate.getDate())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toString())
+
+
+// arrays
+let arr=new Array(1,2,3,4,5,7)
+// console.log(arr[3])
+//few more methods
+// arr.pop()
+// arr.push(5) adds at the end
+// console.log(arr)
+// arr.unshift(22) // adds at the begining
+// console.log(arr)
+// arr.shift() // removes the first element
+// console.log(arr.includes(88))
+// console.log(arr.indexOf(88))
+// const newArr=arr.join()
+// console.log(typeof (arr))
+// console.log(typeof (newArr))
+// slice and splice important
+// const newArr2=arr.slice(1,5) // first included last excluded no change in og arr
+// console.log(newArr2)
+// const newArr3=arr.splice(3,5)// first,last included change in og arr
+// console.log(newArr3)
+// console.log(arr)
+
+// const marvel= new Array("thor", "spriderMan")
+// const dc= new Array("Joker", "BatMan", "superMan")
+// mergeing them ---> push, concat,
+// marvel.push(dc)
+// console.log(marvel)// now array can store multiple types of data so it stored a array inside
+
+// lets some other way
+// const allHeros=marvel.concat(dc) // makes a new array to store
+// console.log(allHeros)
+
+// const anotherArr=[3,2,4,2,5,7,6,3,[5,576,6,332],[5,7,2,[5,3,6]]]
+// const flatArr=anotherArr.flat(Infinity);// use infinity or a number else it will flat upto 1 array only
+// console.log(flatArr)
+//
+// console.log(Array.isArray("Harjot"))
+// console.log(Array.from("Harjot")) // from will make a string array
+// console.log(Array.from({name:"Harjot"})) // will not give the arr directly interseting case
+
+// making an array out of few variables
+// const log1=323;
+// const log2=3;
+// const log3=23;
+// const log4=84;
+// console.log(Array.of(log1,log2,log3,log4))
 
 
 
+// here starts the important part OBJECTS
+const sym1 = Symbol();
+const obj={
+    name:"harjot", "fullName":"Harjot Singh",// one item inside other this is good to know how to access it
+    email:"harjot@apple.com",
+    age:23,
+    isLoggedIn:false,
+    sym1:"yo", // now this is a wrong way to use symbol it will val but the type will be string not symbol/object symbol is also an object
+    [sym1]:"Yo keys not found" // correct format
+}
+// console.log(obj.age)
+// console.log(obj.sym1)
+// console.log(obj[sym1])
+// console.log(obj.fullName)
+
+obj.email="harjot@google.com"
+// Object.freeze(obj) // to make the items fixed cant be changed
+obj.email="harjot@amazon.com"
+//
+// console.log(obj.age)
+// console.log(obj.sym1)
+// console.log(obj[sym1])
+// console.log(obj.fullName)
+// console.log(obj)
+
+// in java script methods are treated as first citizen it means we can easly use them
+obj.greeting= function(){
+    console.log("Hello JS User");
+}
+
+obj.greetingTwo= function(){
+    console.log(`Hello JS User,${this.fullName}`); // now we can call the object reference via this in when we want to use any obj inside val inside a method
+}
+//console.log(obj.greeting());//real way to call a method , it was givng an error due to line 211 y? coz we freeezed the obj so it was saying greeting() is not a method
+//console.log(obj.greeting)// we can also call like this but it will give undefined
+// console.log(obj.greetingTwo());
+
+//more about objects
+//Singleton -- type 1 --> created using new keyword result is same as type 2
+//object literals -- type 2 --> created using the conventional method but still result is same as type 1
+
+//type 1
+const tinderUSer=new Object();
+
+//type 2
+const tinderUSer2={
+}
+//console.log(tinderUSer2)
+//console.log(tinderUSer) // both will give the same result
+
+// we can also add the items in object like this or we can also write them inside the object in a traditonal way
+tinderUSer2.id="234as"
+tinderUSer2.name="Sidak"
+tinderUSer2.isLoggedIn=true;
+// console.log(tinderUSer2)
+
+// tought one object inside object or nested object
+const regularUser={
+    name:{
+        fullName: {
+            firstName: "Harjot",
+            lastName: "Singh"
+        }
+    },
+    email:"harjot@apple.com"
+}
+// console.log(regularUser.name)
+// console.log(regularUser.name.fullName)
+// console.log(regularUser.name.fullName.firstName)
+// console.log(regularUser.name.fullName.lastName)
+// the deeper you dig in the more precise val u get
+// else if you will  not go deeeper its storing the complete obj
+
+ // let see how to combine multiple objects
+const obj11={1:"ssds",5:"sfgsfge"}
+const obj22={2:"bg",7:"e"}
+const obj33={4:"sf",3:"eg"}
+const obj4=Object.assign({},obj11,obj22,obj33); // now here 1st val is target in which all obj will be stored { } use this to make sure we are storing a new obj without altering the any og obj
+// we can use spread operator as same as array
+const obj9={...obj11,...obj22,...obj33}
+//console.log(obj9)
 
 
-
-
+//one more syntax it will be used when we will get data from database , data will be in the form of array of objects
+const useDB= [
+    {
+        id:"1",
+        email:"harjot@apple.com"
+    },
+        {
+            id:"2",
+            email:"harjot@google.com"
+        },
+        {
+            id:"3",
+            email:"harjot@nvdia.com"
+        }
+    ]
+useDB[1].email  // array obj position and now use . to access the inside of the obj
+console.log(tinderUSer2)
+// now we are getting the whole object when we print but we can also get it in array form so that we can later on loop on that array and we can get the desired data out of that array
+console.log(Object.keys(tinderUSer2))
+// we can also abstract values also
+console.log(Object.values(tinderUSer2))
+// now we can also output the array of key value pair in array form too
+console.log(Object.entries(tinderUSer2))
+// we can also check if an object has a particular value stored inside it or not
+console.log(tinderUSer2.hasOwnProperty('isLoggedIn'))
